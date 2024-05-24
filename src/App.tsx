232ff41fee2ec9c45c4ebf2d3ea017db9b26284a
shaifali <shaifali02000@gmail.com>
2024-05-24 16:17:38 +0530

    new story

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import Story from './component/Story';
 // import './App.css';
+import React, { useState } from "react";
+import Story from "./component/Story";
+import { Box, Image, Text, Button } from "@chakra-ui/react";
 
 function App() {
+  const [showStory, setShowStory] = useState(false);
+
+  const handleStartClick = () => {
+    setShowStory(true);
+  };
+
   return (
     <div className="App">
-      <Story/>
-     
+      {!showStory ? (
+        <Box w={{ sm: "70%", base: "100%" }} h="100%">
+          <Image
+            h="100vh"
+            position={'absolute'}
+            src="https://img.freepik.com/free-vector/instagram-logo_1199-122.jpg?t=st=1716540975~exp=1716544575~hmac=e5e988ecc86e3595420b6d3d6c1d97d3b956ee7f5a231138812771aab73d7a22&w=740"
+          />
+          <Button onClick={handleStartClick} position='relative'>Let's Start</Button>
+        </Box>
+      ) : (
+        <Story />
+      )}
     </div>
   );
 }
--- src/component/Story.tsx
+++ src/component/Story.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Image, Modal, ModalOverlay, ModalContent, ModalBody, useDisclosure } from "@chakra-ui/react";
+import { Avatar, Box, Image, Modal, ModalOverlay, ModalContent, ModalBody, useDisclosure, Container } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState, MouseEvent } from "react";
 import { GrLinkPrevious, GrLinkNext } from "react-icons/gr";
@@ -58,8 +58,19 @@ const Story: React.FC = () => {
 
   return (
     <>
-      <Box w="100px" h="100px" onClick={openModal}>
-        {stories.length > 0 && <Avatar src={stories[0].image} />}
+    <Container display='flex' flexDirection='row'>
+        {/* <Image src='https://img.freepik.com/free-psd/social-media-logo-design_23-2151320963.jpg?t=st=1716542783~exp=1716546383~hmac=527914741829c37988e9d2bfaf16970a11492a89b1e48f08eb6a05c5fc3d493a&w=740'/> */}
+        <Image src='https://img.freepik.com/premium-vector/instagram-logo-icon_628407-1924.jpg?w=826'/>
+    </Container>
+      <Box w="100px" h="100px" onClick={openModal} m='1pc' display='flex' flexDirection={'row'} gap='1pc'> 
+        {stories.length > 0 && <Avatar src={stories[0].image}  w="80px" 
+        h="80px" 
+        onClick={openModal} 
+        border={hasOpened ? "4px solid lightgrey" : "4px solid skyblue"}
+        borderColor={hasOpened ? "gray.400" : "blue.500"} 
+        borderRadius="full"
+        cursor="pointer"/>}
+
       </Box>
 
       <Modal isOpen={isOpen} onClose={onClose} size="full">
